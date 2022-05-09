2f1f19c752f6c8d8a11c53c04daf8172566db679
SemenikhinaVioletta <violettas2005@gmail.com>
2022-05-09 21:43:09 +0700

    main4

--- app/src/main/java/com/example/vetraletta/BattleField_activity.java
+++ app/src/main/java/com/example/vetraletta/BattleField_activity.java
@@ -1,7 +1,10 @@
 package com.example.vetraletta;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
@@ -80,6 +83,16 @@ public class BattleField_activity extends MainActivity {
                 }
             }
         }//~Отрисовка поля
+
+        Button button = (Button) findViewById(R.id.end);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BattleField_activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
